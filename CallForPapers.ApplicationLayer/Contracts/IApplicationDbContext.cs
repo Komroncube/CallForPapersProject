3f2a6a35a574093f6f65323b307345ccd5f21f56
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-03-30 16:37:10 +0500

    add contracts

---
+++ CallForPapers.ApplicationLayer/Contracts/IApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using CallForPapers.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CallForPapers.ApplicationLayer.Contracts;
+public interface IApplicationDbContext
+{
+    DbSet<Application> Applications {get; set; }
+}
