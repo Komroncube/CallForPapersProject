14c6e54b5091fc9bbf4d68959e98edd69b43c69f
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-03-30 17:32:35 +0500

    Add ApplicationDbContext and CallForPapers.Infrastructure.csproj

---
+++ CallForPapers.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CallForPapers.ApplicationLayer.Contracts;
+using CallForPapers.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CallForPapers.Infrastructure;
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Application> Applications { get; set; }
+}
